eeb1c3bb8b49bd556f936fafda91beaa5a3687d8
Kirill Bakharovsky <baharovsky@bk.ru>
2021-11-11 23:28:54 +0500

    Initial Commit

---
+++ SwiftUiApp/SwiftUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiAppApp.swift
+//  SwiftUiApp
+//
+//  Created by Кирилл Бахаровский on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
